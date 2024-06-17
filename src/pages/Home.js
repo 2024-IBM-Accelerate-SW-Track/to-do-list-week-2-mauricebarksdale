cf292e75d7d6d8101e9ec14fec9d40fcb432cf9f
mauricebarksdale <momobarksdale@gmail.com>
2024-06-17 17:23:26 -0400

    Finished base features that allow users to delete tasks and prevented duplicates from being created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,13 +20,32 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
+    
+    const found = this.state.todos.find((item) => item.content === todo.content);
+
+    if (found) {
+      return;
+    } else {
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
       todos: new_list,
+      })
+    }
+  };
+
+  // Iterates through todo list to find todo item with matching id and then deletes it from the list.
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
     });
   };
+
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +55,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
